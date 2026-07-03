c9a0fe510391e3a9e217d9cf6741c044eef2f7fa
zero-ljz <2267719005@qq.com>
2026-07-03 21:11:23 +0800

    v0.1.7启动 优化

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import App from "./App";
 import "./index.css";
 
@@ -9,10 +10,36 @@ import "./stores/rpc.store";
 
 import { backendStore } from "./stores/backend.store";
 
+let didBootstrap = false;
+
+async function showMainWindow() {
+  try {
+    const window = getCurrentWindow();
+    await window.show();
+    await window.setFocus();
+  } catch (error) {
+    console.warn("Failed to show main window", error);
+  }
+}
+
+function Root() {
+  React.useEffect(() => {
+    if (didBootstrap) {
+      return;
+    }
+    didBootstrap = true;
+
+    void showMainWindow();
+    setTimeout(() => {
+      void backendStore.init();
+    }, 0);
+  }, []);
+
+  return <App />;
+}
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
-
-void backendStore.init();
